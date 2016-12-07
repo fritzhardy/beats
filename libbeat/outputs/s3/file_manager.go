348c734514e993b737ebac574bdb35970b9eb410
Jeff Hardy <jeff.hardy@spatialkey.com>
2016-12-07 11:46:03 -0500

    Less time punctuation in s3 filenames.

--- libbeat/outputs/s3/file_manager.go
+++ libbeat/outputs/s3/file_manager.go
@@ -170,7 +170,7 @@ func (manager *fileManager) s3KeyName() string {
 
 	t := time.Now().UTC()
 
-	timeIso8601 := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d,%09d+00:00",
+	timeIso8601 := fmt.Sprintf("%d%02d%02dT%02d%02d%02d.%09dZ",
 		t.Year(), t.Month(), t.Day(),
 		t.Hour(), t.Minute(), t.Second(),
 		t.Nanosecond())
